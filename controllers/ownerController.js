b74e0de024d0c658c669f87c518814d9deba5792
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-08-24 16:55:29 -0700

    feat: added Driceora car rental model

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -18,3 +18,16 @@ export const changeRoleToOwner = async (req, res) => {
 };
 
 //API to list Car
+export const addCar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    let car = JSON.parse(req.body.carData)
+    const imageFile = req.file;
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
