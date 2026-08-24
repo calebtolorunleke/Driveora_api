48c569be74dc80109474a8737580950d0e393375
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-08-23 22:31:57 -0700

    Add user authentication routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ import JWT from "jsonwebtoken";
 
 // Generate   JWT Token
 const generateToken = (userId) => {
-  const payload = { userId };
+  const payload = userId;
   return JWT.sign(payload, process.env.JWT_SECRET);
 };
 
@@ -17,24 +17,24 @@ export const registerUser = async (req, res) => {
         success: false,
         message: "fill all the fields",
       });
+    }
 
-      const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ email });
 
-      if (userExists) {
-        return res.json({
-          success: false,
-          message: "User already exist",
-        });
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = await User.create({ name, email, password: hashedPassword });
-      const token = generateToken(user._id.toString());
-      res.json({
-        success: true,
-        token,
+    if (userExists) {
+      return res.json({
+        success: false,
+        message: "User already exist",
       });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+    const token = generateToken(user._id.toString());
+    res.json({
+      success: true,
+      token,
+    });
   } catch (error) {
     console.log(error.message);
     res.json({
