8227a1b6d0900365a3398d50b5e6d4b09100bd44
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-08-24 23:25:43 -0700

    feat: added Driceora car rental model

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -1,3 +1,4 @@
+import { imagekit } from "../configs/imageKit.js";
 import User from "../models/User.js";
 import fs from "fs";
 
@@ -25,7 +26,25 @@ export const addCar = async (req, res) => {
     let car = JSON.parse(req.body.carData);
     const imageFile = req.file;
 
+    //upload image to ImageKit
     const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/cars",
+    });
+
+    // optimization through imagekit URL transformation
+    var imagoptimizedImageURL = imagekit.url({
+      path: response.filepath,
+      transformation: [
+        {
+          height: "300",
+        }, //width resizing
+        { width: "1280" }, //Auto compression
+        { format: "webp" }, //Convert to modern format
+      ],
+    });
   } catch (error) {
     console.log(error.message);
     res.json({
