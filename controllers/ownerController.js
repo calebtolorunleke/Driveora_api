78169657209541bea41579605de3367f5dc4979c
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-08-25 20:53:15 -0700

    Implement api to delete car from admin endpoint

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -145,3 +145,24 @@ export const deletCar = async (req, res) => {
     });
   }
 };
+
+// API to get dashboard data
+export const getDashboardData = async (req, res) => {
+  try {
+    const { _id, role } = req.user;
+    if (role !== "owner") {
+      res.json({
+        success: false,
+        message: "Unauthorised",
+      });
+    }
+
+    const cars = await Car.find({ owner: _id });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
